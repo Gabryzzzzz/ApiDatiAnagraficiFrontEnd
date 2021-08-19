0cb3f8cc48665fde9de2f785bdffdacf68857ede
gabriele.monti24@gmail.com <gabriele.monti24@gmail.com>
2021-08-19 12:56:52 +0200

    Added modal with addData

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgbPaginationModule, NgbAlertModule, NgbModule, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { TableDataComponent } from './table-data/table-data.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ModalNewUser } from './modal-new-user/modal-new-user.component';
+import { BsModalService } from 'ngx-bootstrap/modal'
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TableDataComponent
+    TableDataComponent,
+    NavBarComponent,
+    ModalNewUser
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     ReactiveFormsModule,
+    NgbModule,
+    FormsModule,
   ],
-  providers: [HttpClient],
+  providers: [HttpClient, BsModalService, NgbActiveModal, ReactiveFormsModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
